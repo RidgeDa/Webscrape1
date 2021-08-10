5550676f8b086c56fdac0956952ddd31cc37da03
Ridge Ntim Dampare <ridge_d@hotmail.com>
2021-08-10 22:27:09 +0200

    Added dockerfile and kubernetes MyTEstProject and website_scraping.py

--- website_scraping.py
+++ website_scraping.py
@@ -2,20 +2,21 @@ from bs4 import BeautifulSoup
 import requests
 import time
 
+
 print('Put some skills that you are not familiar with')
 unfamiliar_skill = input('>')
 print(f'Filtering out {unfamiliar_skill}')
 
 def find_jobs():
-    html_text = requests.get('https://www.reed.co.uk/jobs/jobs-in-london').text
-    soup = BeautifulSoup(html_text, 'lxml')
+    html = requests.get('https://www.reed.co.uk/jobs/jobs-in-london').text
+    soup = BeautifulSoup(html, 'html.parser')
     jobs = soup.find_all('div', class_='col-sm-12 col-md-9 col-lg-9 details')
     for index, job in enumerate(jobs):
          company_name = job.find('a', class_='gtmJobListingPostedBy').text.replace(' ','')
          description = job.find('div', class_='description').text.replace(' ','')
          published_date = job.find('div', class_='posted-by').text
          more_info = job.header.a['href']
-         #print(published_date)
+         print(published_date)
          if unfamiliar_skill not in description:
              with open(f'posts/{index}.txt', 'w') as f:
 
